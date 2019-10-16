0e0dbae0738f0be9674bae995f5129b5068b079e
ec17146 <y.zhang@se17.qmul.ac.uk>
2019-10-16 22:07:01 +0100

    version 3

--- C_Sharp_Skeleton_2019/Answers/Question3.cs
+++ C_Sharp_Skeleton_2019/Answers/Question3.cs
@@ -1,13 +1,54 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question3
     {
         public static int Answer(int[] scores, int[] alice)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            //version 3
+            Array.Sort(scores);
+            int lastRank = -1;
+            IDictionary<int, int> dict = new Dictionary<int, int>();
+            int count = 1;
+            int answer = 0;
+            for (int loop = 0; loop<alice.Length;loop++){
+                int rank =1;
+                int i = scores.Length-1;
+                int lastScore = -1;
+                while (i >= 0){
+                    if (i < 0 ){
+                        break;
+                    }
+                    if (alice[loop] < scores[i]){
+                        if(lastScore != scores[i]){
+                            rank ++;
+                        }
+                    }else{
+                        break;
+                    }
+                    lastScore = scores[i];
+                    i--;
+                }
+                if(dict.ContainsKey(rank)){
+                    dict[rank]++;
+                }else{
+                    dict.Add(rank,1);
+                }
+                if(dict[rank] >=count){
+                    if(dict[rank]==count && rank>lastRank){
+                        lastRank = rank;
+                        answer = rank;
+                    }else if(dict[rank]>count){
+                        answer = rank;
+                        count = dict[rank];
+                        
+                    }
+                    
+                }
+            }
+            
+            return answer;
         }
     }
 }
