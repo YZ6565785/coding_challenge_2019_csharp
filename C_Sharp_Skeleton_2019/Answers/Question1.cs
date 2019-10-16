46697e376c8d0e94ad974504ddb2c7979864a122
ec17146 <y.zhang@se17.qmul.ac.uk>
2019-10-16 17:42:43 +0100

    version3

--- C_Sharp_Skeleton_2019/Answers/Question1.cs
+++ C_Sharp_Skeleton_2019/Answers/Question1.cs
@@ -10,14 +10,15 @@ namespace C_Sharp_Challenge_Skeleton.Answers
     {
         public static int Answer(double initialLevelOfDebt, double interestPercentage, double repaymentPercentage)
         {
+            //version3
             double p = 0.01;
-            double repayment = initialLevelOfDebt*repaymentPercentage*p;
-            double answer = initialLevelOfDebt*10*p;
-            while(repayment<=initialLevelOfDebt){
-                initialLevelOfDebt = initialLevelOfDebt*(1+(interestPercentage)*p) - repayment;
-                answer = answer + repayment;
-            }
-            return Convert.ToInt16(Math.Round(answer + initialLevelOfDebt));
+            double repayment = initialLevelOfDebt*repaymentPercentage*p;
+            double answer = initialLevelOfDebt*10*p;
+            while(repayment<=initialLevelOfDebt){
+                initialLevelOfDebt = initialLevelOfDebt*(1+(interestPercentage)*p) - repayment;
+                answer = answer + repayment;
+            }
+            return Convert.ToInt16(answer + initialLevelOfDebt);
 
         }
     }
