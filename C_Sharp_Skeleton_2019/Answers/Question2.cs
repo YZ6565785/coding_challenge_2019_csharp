cce02532b2f23ca7132cb5755ff14b6b6d988f86
ec17146 <y.zhang@se17.qmul.ac.uk>
2019-10-17 01:05:42 +0100

    version 11

--- C_Sharp_Skeleton_2019/Answers/Question2.cs
+++ C_Sharp_Skeleton_2019/Answers/Question2.cs
@@ -1,69 +1,73 @@
-﻿namespace C_Sharp_Challenge_Skeleton.Answers
+﻿using System;
+using System.Collections.Generic;
+namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question2
     {
         public static int Answer(int[] risk, int[] bonus, int[] trader){
-            //VERSION 10
-        //speed: 0.0729
-        int answer = 0;
-        int lowestRiskValue = risk[0];
-        foreach(int item in risk)
-        {
-            if(item<lowestRiskValue){
-                lowestRiskValue = item;
+            //VERSION 11
+            //speed: 0.24999
+            int maxIndex = trader.Length;
+            int answer = 0;
+            Array.Sort(trader);
+            int lowestRiskValue = risk[0];
+            foreach(int item in risk)
+            {
+                if(item<lowestRiskValue){
+                    lowestRiskValue = item;
+                }
             }
-        }
-        bool conti = true;
-        while(conti){
+            bool conti = true;
+            while(conti){
+                int highestBonus =0;
+                int count =0;
+                conti = false;
+                int i = 1;
             
-
-            int highestBonus =0;
-            int count =0;
-            conti = false;
-            int i = 1;
-           
-            while(i<bonus.Length){
-                if(bonus[i] != -1){
-                    if  (bonus[highestBonus] <= bonus[i]){
-                        if(risk[highestBonus]>risk[i] || (bonus[highestBonus] < bonus[i] && risk[highestBonus]==risk[i]) ){
-                            bonus[highestBonus] = -1;
-                            highestBonus = i;
-                        }else if(bonus[highestBonus] == bonus[i] && (risk[highestBonus]==risk[i] || risk[highestBonus]<risk[i]) ){
-                            bonus[i] = -1;
+                while(i<bonus.Length){
+                    if(bonus[i] != -1){
+                        if  (bonus[highestBonus] <= bonus[i]){
+                            if(risk[highestBonus]>risk[i] || (bonus[highestBonus] < bonus[i] && risk[highestBonus]==risk[i]) ){
+                                bonus[highestBonus] = -1;
+                                highestBonus = i;
+                            }else if(bonus[highestBonus] == bonus[i] && (risk[highestBonus]==risk[i] || risk[highestBonus]<risk[i]) ){
+                                bonus[i] = -1;
+                            }else{
+                                highestBonus = i;
+                            }
                         }else{
-                            highestBonus = i;
-                        }
-                    }else{
-                        if(risk[highestBonus]==risk[i] || risk[highestBonus]<risk[i] ){
-                            bonus[i] = -1;
+                            if(risk[highestBonus]==risk[i] || risk[highestBonus]<risk[i] ){
+                                bonus[i] = -1;
+                            }
                         }
                     }
-                }
-                
+                    
 
-                i++;
-            }
-            int j = 0;
-            while(j<trader.Length){
-                if(trader[j] != -1){
+                    i++;
+                }
+                int j = 0;
+                while(j<maxIndex){
                     if (trader[j] >= risk[highestBonus]){
                         trader[j] = -1;
-                        count++;
+                        count+=maxIndex - j;
+                        maxIndex = j;
+                        break;
                     }
                     if (!(conti)){
                         if(trader[j]>=lowestRiskValue){
                             conti = true;
                         } 
                     }
+                    j++;
+                    
                 }
-                j++;
+                answer += (count)*bonus[highestBonus];
+                bonus[highestBonus] = -1;
                 
             }
-            answer += (count)*bonus[highestBonus];
-            bonus[highestBonus] = -1;
             
-        }
-        return answer;
+
+            return answer;
         }
 
     }
