19597e85ab8f39c679bd59722065a65041e8a2db
ec17146 <y.zhang@se17.qmul.ac.uk>
2019-10-17 01:33:17 +0100

    version 11

--- C_Sharp_Skeleton_2019/Answers/Question2.cs
+++ C_Sharp_Skeleton_2019/Answers/Question2.cs
@@ -6,25 +6,28 @@ namespace C_Sharp_Challenge_Skeleton.Answers
     {
         public static int Answer(int[] risk, int[] bonus, int[] trader){
             //VERSION 11
-            //speed: 0.24999
+            //speed: 0.24400
             int maxIndex = trader.Length;
             int answer = 0;
             Array.Sort(trader);
-            int lowestRiskValue = risk[0];
-            foreach(int item in risk)
-            {
-                if(item<lowestRiskValue){
-                    lowestRiskValue = item;
-                }
-            }
+            int lowestRiskValue = 9999;
+            bool foundLowestRisk = false;
             bool conti = true;
             while(conti){
+                if(lowestRiskValue != 9999){
+                    foundLowestRisk = true;
+                }
                 int highestBonus =0;
                 int count =0;
                 conti = false;
                 int i = 1;
             
                 while(i<bonus.Length){
+                    if (!foundLowestRisk){
+                        if(risk[i]<lowestRiskValue){
+                            lowestRiskValue = risk[i];
+                        }
+                    }
                     if(bonus[i] != -1){
                         if  (bonus[highestBonus] <= bonus[i]){
                             if(risk[highestBonus]>risk[i] || (bonus[highestBonus] < bonus[i] && risk[highestBonus]==risk[i]) ){
@@ -48,7 +51,6 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                 int j = 0;
                 while(j<maxIndex){
                     if (trader[j] >= risk[highestBonus]){
-                        trader[j] = -1;
                         count+=maxIndex - j;
                         maxIndex = j;
                         break;
@@ -65,8 +67,6 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                 bonus[highestBonus] = -1;
                 
             }
-            
-
             return answer;
         }
 
