0a6963264453f05204ee5524b47f7e1eb4bc5c1a
ec17146 <y.zhang@se17.qmul.ac.uk>
2019-10-18 16:16:37 +0100

    version 1

--- C_Sharp_Skeleton_2019/Answers/Question6.cs
+++ C_Sharp_Skeleton_2019/Answers/Question6.cs
@@ -4,8 +4,26 @@
     {
         public static int Answer(string[] input)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            //version 1
+            int answer = 0;
+            bool getSameStart = false;
+            string start = input[0];
+            int lastI = start.Length-1;
+            for (int i = 1; i<input.Length; i++){
+                if(start[0].Equals(input[i][0])){
+                    if(!getSameStart){
+                        getSameStart = true;
+                    }
+                    
+                    if(start[lastI].Equals(input[i][input[i].Length-1])){
+                        answer = i;
+                    }
+                }
+            }      
+            if(answer ==0 && getSameStart){
+                answer = -1;
+            }
+            return answer;
         }
     }
 }
