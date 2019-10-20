e8f6da248f40964d21ce7909476603bbe8ae79a0
ec17146 <y.zhang@se17.qmul.ac.uk>
2019-10-20 23:12:23 +0100

    test

--- C_Sharp_Skeleton_2019/Answers/Question2.cs
+++ C_Sharp_Skeleton_2019/Answers/Question2.cs
@@ -1,11 +1,72 @@
-﻿namespace C_Sharp_Challenge_Skeleton.Answers
+﻿using System;
+using System.Collections.Generic;
+namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question2
     {
-        public static int Answer(int[] risk, int[] bonus, int[] trader)
-        {
-            //TODO: Please work out the solution;
-            return -1;
+        public static int Answer(int[] risk, int[] bonus, int[] trader){
+            //VERSION 12
+            //speed: 0.23661
+            int maxIndex = trader.Length;
+            int answer = 0;
+            Array.Sort(trader);
+            int lowestRiskValue = risk[0];
+            foreach(int item in risk)
+            {
+                if(item<lowestRiskValue){
+                    lowestRiskValue = item;
+                }
+            }
+            bool conti = true;
+            while(conti){
+                int highestBonus =0;
+                int count =0;
+                conti = false;
+                int i = 1;
+                while(i<bonus.Length){
+                    if(bonus[i] != -1){
+                        if(bonus[highestBonus] < bonus[i]){
+                            if(risk[highestBonus]>=risk[i]){
+                                bonus[highestBonus] = -1;
+                                highestBonus = i;
+                            }else{
+                                highestBonus = i;
+                            }
+                        }else{
+                            if (risk[highestBonus]<=risk[i]){
+                                bonus[i] = -1;
+                            }else if(bonus[highestBonus] == bonus[i]&&risk[highestBonus]>risk[i]){
+                                bonus[highestBonus] = -1;
+                                highestBonus = i;
+                            }
+                            
+                        }
+                    }
+                    
+
+                    i++;
+                }
+                int j = 0;
+                while(j<maxIndex){
+                    if (trader[j] >= risk[highestBonus]){
+                        count+=maxIndex - j;
+                        maxIndex = j;
+                        break;
+                    }
+                    if (!(conti)){
+                        if(trader[j]>=lowestRiskValue){
+                            conti = true;
+                        } 
+                    }
+                    j++;
+                    
+                }
+                answer += (count)*bonus[highestBonus];
+                bonus[highestBonus] = -1;
+                
+            }
+            return answer;
         }
+
     }
 }
--- C_Sharp_Skeleton_2019/Answers/Question4.cs
+++ C_Sharp_Skeleton_2019/Answers/Question4.cs
@@ -4,8 +4,63 @@
     {
         public static int Answer(int[] v, int[] c, int mc)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            //version 3 greedy
+            quickSort2(v,0,v.Length-1,c);
+            int answer = 0;
+            for(int i=v.Length-1;i>=0;i--){
+                if(mc==0){
+                    break;
+                }
+                if(c[i]<=mc){
+                    mc -=c[i];
+                    answer+=v[i];
+                }
+            }
+            return answer;
+        }
+        
+        
+
+        static void quickSort2(int[] arr, int low, int high, int[] arr2){
+		
+            if (arr == null || arr.Length == 0){
+                return;
+            }
+            if (low >= high){
+                return;
+            }
+            // pick the pivot
+            int middle = low + (high - low) / 2;
+            int pivot = arr[middle];
+            // make left < pivot and right > pivot
+            int i = low, j = high;
+            while (i <= j) {
+                while (arr[i] < pivot) {
+                    i++;
+                }
+
+                while (arr[j] > pivot) {
+                    j--;
+                }
+
+                if (i <= j) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    int temp2 = arr2[i];
+                    arr2[i] = arr2[j];
+                    arr2[j] = temp2;
+                    i++;
+                    j--;
+                }
+            }
+            // recursively sort two sub parts
+            if (low < j)
+                quickSort2(arr, low, j, arr2);
+
+            if (high > i)
+                quickSort2(arr, i, high, arr2);
+            //
         }
     }
 }
