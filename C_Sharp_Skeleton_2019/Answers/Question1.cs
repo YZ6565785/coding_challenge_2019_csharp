631a228df8e878a58c24aca1b1400ca7f7d31624
ec17146 <y.zhang@se17.qmul.ac.uk>
2019-10-20 21:02:11 +0100

    re-push for q1 version 3.1

--- C_Sharp_Skeleton_2019/Answers/Question1.cs
+++ C_Sharp_Skeleton_2019/Answers/Question1.cs
@@ -1,17 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Threading.Tasks;
-
 namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question1
     {
         public static int Answer(double initialLevelOfDebt, double interestPercentage, double repaymentPercentage)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            //version 3.1.0
+            //speed: 0.0039
+            double p = 0.01;
+            double repayment = initialLevelOfDebt*repaymentPercentage*p;
+            double answer = initialLevelOfDebt*10*p;
+            while(repayment<initialLevelOfDebt){
+                initialLevelOfDebt *= initialLevelOfDebt *(1+interestPercentage*p) - repayment;
+                answer += repayment;
+            }
+            return Convert.ToInt16(answer + initialLevelOfDebt);
+
         }
     }
 }
