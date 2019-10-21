b32d271af2e191c27caf64b2c0572faa1bca532e
ec17146 <y.zhang@se17.qmul.ac.uk>
2019-10-21 19:56:02 +0100

    debugged version 4 memo

--- C_Sharp_Skeleton_2019/Answers/Question4.cs
+++ C_Sharp_Skeleton_2019/Answers/Question4.cs
@@ -7,21 +7,21 @@
             //version 4 memo version
             int i, j; 
             int length = v.Length;
-            int[,] memo = new int[length+1][mc+1]; 
+            int[,] memo = new int[length+1, mc+1]; 
             for (i = 0; i <= length; i++){ 
                 for (j = 0; j <= mc; j++){ 
                     if (i==0 || j==0){
-                        memo[i][j] = 0; 
+                        memo[i, j] = 0; 
                     }else if (c[i-1] <= j){
-                        int a = v[i-1] + memo[i-1][j-c[i-1]];
-                        int b = memo[i-1][j];
-                        memo[i][j] = (a>b)?a:b;
+                        int a = v[i-1] + memo[i-1, j-c[i-1]];
+                        int b = memo[i-1, j];
+                        memo[i, j] = (a>b)?a:b;
                     }else{
-                        memo[i][j] = memo[i-1][j]; 
+                        memo[i, j] = memo[i-1, j]; 
                     }
                 } 
             } 
-            return memo[length][mc]; 
+            return memo[length, mc]; 
         }
     }
 }
