78f1eff8b608fa70cdf1dd84c70c315e471fbcdf
ec17146 <y.zhang@se17.qmul.ac.uk>
2019-10-22 16:47:42 +0100

    version 15

--- C_Sharp_Skeleton_2019/Answers/Question2.cs
+++ C_Sharp_Skeleton_2019/Answers/Question2.cs
@@ -6,60 +6,33 @@ namespace C_Sharp_Challenge_Skeleton.Answers
     public class Question2
     {
         public static int Answer(int[] risk, int[] bonus, int[] trader){
-            //VERSION 14
-            //speed: 0.223
+            //VERSION 15
+            //speed: 0.156127
             int maxIndex = trader.Length-1;
             int answer = 0;
             Array.Sort(trader);
-            int lowestRiskValue = risk[0];
+            Array.Sort(bonus,risk);
             int length = risk.Length;
-            int m, i, j;
-            for(m =1; m<length; m ++){
-                if(risk[m]<lowestRiskValue){
-                    lowestRiskValue = risk[m];
-                }
-            }
-            bool conti = true;
-            while(conti){
-                int highestBonus =0;
-                int count =0;
-                conti = false;
-                for(i =1; i<length; i ++){
-                    if(bonus[i] != -1){
-                        if(bonus[highestBonus] < bonus[i]){
-                            if(risk[highestBonus]>=risk[i]){
-                                bonus[highestBonus] = -1;
-                                highestBonus = i;
-                            }else{
-                                highestBonus = i;
-                            }
+            int i, j;
+            int lastRisk = 999;
+            for(i =length-1; i>=0; i--){
+                if(risk[i]>=lastRisk){
+                    continue;
+                }else{
+                    int count =0;
+                    lastRisk = risk[i];
+                    for(j =maxIndex; j>=0; j--){
+                        if (trader[j] >= risk[i]){
+                            count++;
                         }else{
-                            if (risk[highestBonus]<=risk[i]){
-                                bonus[i] = -1;
-                            }else if(bonus[highestBonus] == bonus[i]&&risk[highestBonus]>risk[i]){
-                                bonus[highestBonus] = -1;
-                                highestBonus = i;
-                            }
-                            
+                            maxIndex = j;
+                            break;
                         }
                     }
+                    answer += (count)*bonus[i];
                 }
-                for(j =maxIndex; j>=0; j--){
-                    if (trader[j] >= risk[highestBonus]){
-                        count++;
-                    }else{
-                        if(trader[j]>=lowestRiskValue){
-                            conti = true;
-                        } 
-                        maxIndex = j;
-                        break;
-                    }
-                }
-                answer += (count)*bonus[highestBonus];
-                bonus[highestBonus] = -1;
             }
             return answer;
         }
-
     }
 }
