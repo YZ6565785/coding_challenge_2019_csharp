8f207ab500b9dc12d437ce9c0b147f7a1a7a3be6
ec17146 <y.zhang@se17.qmul.ac.uk>
2019-10-22 16:52:36 +0100

    version 4 greedy

--- C_Sharp_Skeleton_2019/Answers/Question4.cs
+++ C_Sharp_Skeleton_2019/Answers/Question4.cs
@@ -4,8 +4,8 @@
     {
         public static int Answer(int[] v, int[] c, int mc)
         {
-            //version 3 greedy
-            quickSort2(v,0,v.Length-1,c);
+            //version 4 greedy
+            Array.Sort(v,c);
             int answer = 0;
             for(int i=v.Length-1;i>=0;i--){
                 if(mc==0){
@@ -17,48 +17,6 @@
                 }
             }
             return answer;
-        }
-        
-        
-
-        static void quickSort2(int[] arr, int low, int high, int[] arr2){
-		
-            if (arr == null || arr.Length == 0){
-                return;
-            }
-            if (low >= high){
-                return;
-            }
-            // pick the pivot
-            int middle = low + (high - low) / 2;
-            int pivot = arr[middle];
-            // make left < pivot and right > pivot
-            int i = low, j = high;
-            while (i <= j) {
-                while (arr[i] < pivot) {
-                    i++;
-                }
-
-                while (arr[j] > pivot) {
-                    j--;
-                }
-
-                if (i <= j) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    int temp2 = arr2[i];
-                    arr2[i] = arr2[j];
-                    arr2[j] = temp2;
-                    i++;
-                    j--;
-                }
-            }
-            if (low < j)
-                quickSort2(arr, low, j, arr2);
-
-            if (high > i)
-                quickSort2(arr, i, high, arr2);
         }
     }
 }
